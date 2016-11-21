8aa0e666d818a8e0906d2b8439e438b49a1bd27a
Anthony <anthonydevenuto@gmail.com>
2016-11-20 22:35:50 -0600

    found and fixed edge case/text higlighted before append

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,8 @@
-function appendChar(char, start) {
+function appendChar(char, start, end) {
   var display = $('#user-query-show');
+  if (start !== end) {
+    alterString(start, end)
+  }
   if (display.text().length === start) {
     display.append(char);
   } else {
@@ -21,25 +24,23 @@ function removeChar(start) {
   }
 }
 
-function alterString() {
-  var input = document.getElementById('input-field');
+function alterString(start, end) {
   var display = $('#user-query-show');
-  var start = input.selectionStart;
-  var end = input.selectionEnd;
-  // call removeChar() if text is not highlighted
-  if (start === end && start !== 0) {
-    removeChar(start);
+  var pos1 = start;
+  var pos2 = end;
+  if (pos1 === pos2 && pos1 !== 0) {
+    removeChar(pos1);
     // remove highlighted char's from the front
-  } else if (start === 0) {
-    var rmFront = display.text().substring(end);
+  } else if (pos1 === 0) {
+    var rmFront = display.text().substring(pos2);
     display.html(rmFront);
     // remove highlighted char's from the end
-  } else if (display.text().length === end) {
-    var rmEnd = display.text().slice(0, start);
+  } else if (display.text().length === pos2) {
+    var rmEnd = display.text().slice(0, pos1);
     display.html(rmEnd);
   } else {
     // remove highlighted char's from the middle
-    var rmMiddle = display.text().slice(0, start) + display.text().substring(end);
+    var rmMiddle = display.text().slice(0, pos1) + display.text().substring(pos2);
     display.html(rmMiddle);
   }
 }
@@ -48,12 +49,16 @@ $(document).on('keypress', function(e) {
   var input = document.getElementById('input-field');
   // pass this to appendChar() to insert char's into the middle when cursor is not at the end
   var start = input.selectionStart;
+  var end = input.selectionEnd;
   if (e.which !== 13 && $('#input-field').is(':focus')) {
-    appendChar(String.fromCharCode(e.which), start);
+    appendChar(String.fromCharCode(e.which), start, end);
   }
 });
 $(document).on('keydown', function(e) {
+  var input = document.getElementById('input-field');
+  var start = input.selectionStart;
+  var end = input.selectionEnd;
   if (e.which === 8 && $('#input-field').is(':focus')) {
-    alterString()
+    alterString(start, end)
   }
 });
