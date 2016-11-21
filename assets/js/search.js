0177df4c3f38d02be837a6aa60fcd22a01778a0d
Anthony <anthonydevenuto@gmail.com>
2016-11-21 12:59:55 -0600

    begining to work on design

--- assets/js/search.js
+++ assets/js/search.js
@@ -3,7 +3,7 @@
             var inputField = $('#input-field').val();
             if (e.which === 13 && inputField !== "") {
               $.ajax({
-                  url: 'https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=extracts%7Cpageimages&generator=search&gsrsearch='+inputField+'&redirects=1&exlimit=10&exintro=1&piprop=thumbnail&pithumbsize=240&pilimit=10&grnnamespace=0&grnlimit=10",true)',
+                  url: 'https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=extracts%7Cpageimages&generator=search&gsrlimit=20&gsrsearch='+inputField+'&redirects=1&exlimit=20&exintro=1&piprop=thumbnail&pithumbsize=240",true)',
                   data: {
                           // action: 'query',
                           // list: 'search',
@@ -25,29 +25,15 @@
     var pages   = apiResult.query.pages;
     var tracker = Object.keys(pages);
 
-
-    for(var i = 0;i<tracker.length;i++) {
-
-      var p1 = tracker[0]
-      var p2 = tracker[1]
-      $('#display').append('<div class="result row">' +
-                            '<div class="col-sm-6 page">' +
-                               '<a href="https://en.wikipedia.org/wiki/' + pages[p1].title + '">' +
-                               '<h3>' + pages[p1].title + '</h3>' +
-                               '<p>' + pages[p1].extract + '</p>' +
-                               // '<img src=' + pages[p1].thumbnail.source + '>' +
+    tracker.forEach(function(key) {
+      $('#display').append('<div class="page item">' +
+                               '<a href="https://en.wikipedia.org/wiki/' + pages[key].title + '">' +
+                               '<h3>' + pages[key].title + '</h3>' +
+                               '<p>' + pages[key].extract + '</p>' +
+                               // '<img src=' + pages[key].thumbnail.source + '>' +
                                '</a>' +
-                             '</div>' +
-                             '<div class="col-sm-6 page">' +
-                               '<a href="https://en.wikipedia.org/wiki/' + pages[p2].title + '">' +
-                               '<h3>' + pages[p2].title + '</h3>' +
-                               '<p>' + pages[p2].extract + '</p>' +
-                               // '<img src=' + pages[p2].thumbnail.source + '>' +
-                               '</a>' +
-                             '</div>' +
                             '</div>');
-      tracker.splice(0,2);
-    }
+    })
   }
 
 
