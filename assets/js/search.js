fc92155945fb67917e7a0393e6c55cdb2877dc4e
Anthony <anthonydevenuto@gmail.com>
2016-11-21 19:37:15 -0600

    added random functionality

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,24 +1,35 @@
-    $(document).ready(function(){
-        $(document).on('keypress', function(e) {
-            var inputField = $('#input-field').val();
-            if (e.which === 13 && inputField !== "") {
-              $.ajax({
-                  url: 'https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=extracts%7Cpageimages&generator=search&gsrlimit=20&gsrsearch='+inputField+'&redirects=1&exlimit=20&exintro=1&piprop=thumbnail&pithumbsize=240",true)',
-                  data: {
-                          // action: 'query',
-                          // list: 'search',
-                          // srsearch: inputField,
-                          // format: 'json'
-                        },
-                  dataType: 'jsonp',
-                  success: function (data) {
-                    console.log(data);
-                    processResult(data);
-                  }
-              });
-            }
-        });
+
+  $(document).on('keypress', function(e) {
+    var inputField = $('#input-field').val();
+    if (e.which === 13 && inputField !== "") {
+      wikiReq(true, inputField);
+    }
+  });
+  $('.fa-search').on('mousedown', function(){
+    var inputField = $('#input-field').val();
+    if (inputField !== "") {
+      wikiReq(true, inputField);
+    }
+  })
+  $('#rand').on('mousedown', function(){
+    var inputField = $('#input-field').val();
+      wikiReq(false, inputField);
+  })
+  function wikiReq(isRand, inputField) {
+    var searchType;
+    var search = 'https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=extracts%7Cpageimages&generator=search&gsrlimit=20&gsrsearch='+inputField+'&redirects=1&exlimit=20&exintro=1&piprop=thumbnail&pithumbsize=240",true)';
+    var random = 'https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=extracts%7Cpageimages&generator=random&redirects=1&exlimit=20&exintro=1&piprop=thumbnail&pithumbsize=240&pilimit=20&grnnamespace=0&grnlimit=20",true)';
+    if (isRand) {searchType = search} else {searchType = random};
+    $.ajax({
+        url: searchType,
+        data: {},
+        dataType: 'jsonp',
+        success: function (data) {
+          console.log(data);
+          processResult(data);
+        }
     });
+  }
 
   function processResult(apiResult){
     $('#display').empty();
@@ -28,10 +39,10 @@
     tracker.forEach(function(key) {
       $('#display').append('<div class="page item">' +
                                '<a href="https://en.wikipedia.org/wiki/' + pages[key].title + '">' +
-                               '<h3>' + pages[key].title + '</h3>' +
-                               '<p>' + pages[key].extract + '</p>' +
+                               '<h1>' + pages[key].title + '</h1>' +
                                // '<img src=' + pages[key].thumbnail.source + '>' +
                                '</a>' +
+                               '<p>' + pages[key].extract + '</p>' +
                             '</div>');
     })
   }
