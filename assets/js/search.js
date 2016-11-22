9c8b727647e00bb973d51240f19801880118c9f2
Anthony <anthonydevenuto@gmail.com>
2016-11-21 23:16:13 -0600

    added images to results

--- assets/js/search.js
+++ assets/js/search.js
@@ -17,7 +17,7 @@
   })
   function wikiReq(isRand, inputField) {
     var searchType;
-    var search = 'https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=extracts%7Cpageimages&generator=search&gsrlimit=20&gsrsearch='+inputField+'&redirects=1&exlimit=20&exintro=1&piprop=thumbnail&pithumbsize=240",true)';
+    var search = 'https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=extracts%7Cpageimages&generator=search&redirects=1&exlimit=10&exintro=1&piprop=thumbnail&pithumbsize=240&pilimit=10&gsrsearch=' +inputField+ '&gsrnamespace=0&gsrlimit=10&gsrenablerewrites=1';
     var random = 'https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&prop=extracts%7Cpageimages&generator=random&redirects=1&exlimit=20&exintro=1&piprop=thumbnail&pithumbsize=240&pilimit=20&grnnamespace=0&grnlimit=20",true)';
     if (isRand) {searchType = search} else {searchType = random};
     $.ajax({
@@ -38,10 +38,11 @@
 
     tracker.forEach(function(key) {
       var k = key;
+      var img = pages[k].thumbnail;
       $('#display').append('<div class="page item">' +
                                '<a href="https://en.wikipedia.org/wiki/' + pages[k].title + '">' +
                                '<h1>' + pages[k].title + '</h1>' +
-                               // '<img src=' + pages[k].thumbnail.source + '>' +
+                                  (img ? '<img src=' + pages[k].thumbnail.source + '>' : '') +
                                '</a>' +
                                '<p>' + pages[k].extract + '</p>' +
                             '</div>');
